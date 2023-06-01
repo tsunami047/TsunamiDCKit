187ef527b0305f79f06dfcc0625f415dce2224d7
Tsunami047 <sussicran@proton.me>
2023-06-01 17:43:16 +0800

    First of all

---
+++ src/main/java/git/tsunami047/tsunamidckit/TsunamiDCKit.java
@@ -0,0 +1,17 @@
+package git.tsunami047.tsunamidckit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TsunamiDCKit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
